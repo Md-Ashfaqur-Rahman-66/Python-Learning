d2144e13ba5a6214891b5d00de18f994c9a2e9dd
Md. Ashfaqur Rahman <arluban359@gmail.com>
2025-04-11 15:04:05 +0600

    Update ChineseChef.py

--- ChineseChef.py
+++ ChineseChef.py
@@ -1,8 +1,1 @@
-from Chef import Chef
-class ChineseChef(Chef):
 
-    def make_special_dish(self):                #overrighting the 'make_special_dish' function of 'Chef' class
-        print("The Chef makes orange Chicken")
-
-    def make_fried_rice(self):
-        print("The chef makes fried rice")
